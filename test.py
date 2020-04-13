df654c6adbbfd8ad5fd589cdb404e5dca401993b
Kungking Chaisilwattana <33508730+kungkingc@users.noreply.github.com>
2020-04-14 02:19:08 +0700

    Update test for sample.py

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
-def test_print:
-  print("Hello world")
+from sample import sum, sum_only_positive
+
+def test_sum():
+    assert sum(5, 5) == 10
+
+def test_sum_positive_ok():
+    assert sum_only_positive(2, 2) == 4
+
+def test_sum_positive_fail():
+    assert sum_only_positive(-1, 2) is None
