f9f641b2962a35e13fc4d82f68aaa03c49d2e8a8
Kungking Chaisilwattana <33508730+kungkingc@users.noreply.github.com>
2020-04-15 19:51:41 +0700

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,7 +1,6 @@
 def sum(num1, num2):
      return num1 + num2
-
- def sum_only_positive(num1, num2):
+def sum_only_positive(num1, num2):
      if num1 > 0 and num2 > 0:
          return num1 + num2
      else:
